cfc8dfa1f024d669c1e466084b9c7437bb8069ce
elminhoemmanuel <amodue15@gmail.com>
2022-07-13 21:18:24 -0500

    fixed fixture bug

--- project-2/client/cypress/integration/rewards.spec.js
+++ project-2/client/cypress/integration/rewards.spec.js
@@ -13,10 +13,11 @@ describe("Rewards dashboard", () => {
     })
 
     it("should display a list of rewards using a mock", ()=>{
-        cy.intercept("GET", "http://localhost:4000/rewards", {fixture:"rewards.json"})
+        cy.intercept("GET", "http://localhost:4000/rewards", {fixture:"rewards"})
+        cy.visit('/rewards');
         cy.get("ul")
-        .should("contain", "500 points for drinking 8 cups of water for 7 straight days")
-        .and("contain","850 points for fasting for 5 days straight")
+        .should("contain", "501 points for drinking 8 cups of water for 7 straight days")
+        .and("contain","851 points for fasting for 5 days straight")
     })
 
 })
